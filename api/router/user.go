e6c776727c5674590073bb196fce14a79482e1fe
Gresham <1543732388@qq.com>
2024-03-16 09:58:17 +0800

    fix(router/user)：fix email verification api

--- api/router/user.go
+++ api/router/user.go
@@ -15,7 +15,7 @@ func InitUser(g *echo.Group) {
 	g.POST("/login", controller.Login)
 	g.POST("/email", controller.RegisterEmail)
 	g.POST("/email_login", controller.LoginWithEmail)
-	g.POST("/verification_code", controller.SendVerificationCode)
+	g.POST("/email_verification_code", controller.SendVerificationCode)
 
 	g.DELETE("/", controller.DeleteUser)
 
